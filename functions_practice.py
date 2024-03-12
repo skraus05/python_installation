ab731f830e2e918fb3d81a2d5d154743afb46a76
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-03-11 21:23:24 -0400

    completed installation of python and practice examples

---
+++ functions_practice.py
@@ -0,0 +1,19 @@
+def hello():
+    print("what's up?")
+
+def pack(element_1, element_2, element_3):
+    return [element_1, element_2, element_3]
+
+def eat_lunch(list_of_food):
+    if len(list_of_food) == 0:
+        print("My lunchbox is empty!")
+    else:
+        for i in range(len(list_of_food)):
+            if i = 0:
+                print(f"First I eat {list_of_food[0]}")
+            else:
+                print(f"Next I eat {list_of_food[i]}")
+
+hello()
+print(pack("element_1", "element_2", "element_3"))
+eat_lunch([])
